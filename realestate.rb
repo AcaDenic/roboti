f136deb08f974aa24e0b5f447ee1506d1377ab5e
Aleksandar <alekpalek@gmail.com>
2013-11-20 22:49:32 +0100

    Dodata neispravna verzija robota - realstate.

---
+++ realestate.rb
@@ -0,0 +1,49 @@
+require "capybara/dsl"
+require "spreadsheet"
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium
+Capybara.default_selector = :xpath
+Spreadsheet.client_encoding = 'UTF-8'
+
+class Realestate
+	include Capybara::DSL
+
+	def initialize
+		@excel = Spreadsheet::Workbook.new
+		@radni_list = @excel.create_worksheet
+		@red = 0
+	end
+
+	def go
+		poseti_linkove
+		sacuvaj_podatke
+	end
+
+	def poseti_linkove
+		visit "http://www.realestate.com.au/buy/property-house/list-1"
+		details = all("//a[@class='detailsButton button']")
+		linkovi = []
+
+		details.each do |a|
+			linkovi << a[:href]
+		end
+
+		linkovi.each do |link|
+			visit link
+		end
+	end
+
+	def sacuvaj_podatke
+		podaci = all("//div[@class='agent']")
+		podaci.each do |podatak|
+			@radni_list[@red, 0] = podatak.find("./div[@class='agentContactInfo']/p[@class='agentName']/strong").text
+			@red = @red + 1
+		end
+		@excel.write "realestate.xls"
+	end
+
+end
+
+realestate = Realestate.new
+realestate.go
