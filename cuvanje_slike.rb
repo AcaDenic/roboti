6d6876c5b67b231cd8de335fe36182e7d2c398d6
Aleksandar <alekpalek@gmail.com>
2013-11-23 00:21:49 +0100

    Dodat nedovrsen robot za skidanje slika koji koristi modul.

--- cuvanje_slike.rb
+++ cuvanje_slike.rb
@@ -1,5 +1,20 @@
 require 'open-uri'
 
-open("harmonika.jpg", 'wb') do |file|
-  file << open("http://www.musicjunction.com.au/WebRoot/ecshared01/Shops/musicjunction/4F9E/0F70/076B/0BEA/58BF/C0A8/D240/245E/Guerrini_President_Oro-2.jpg").read
+module Downloadable
+	def preuzmi_sliku(url)
+		open(url, 'wb') do |file|
+ 		file << open(url).read
+		end
+	end
 end
+
+class ProbaModula
+	include Downloadable
+	def go
+		preuzmi_sliku("http://www.musicjunction.com.au/WebRoot/ecshared01/Shops/musicjunction/4F9E/0F70/076B/0BEA/58BF/C0A8/D240/245E/Guerrini_President_Oro-2.jpg")
+	end
+end
+
+proba_modula = ProbaModula.new
+proba_modula.go
+
